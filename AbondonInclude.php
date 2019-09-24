f5c7d63bfc1001aebcba0306058ac7aa4b06369e
Hiroyuki Yakushi <yakushi@forblu.com>
2019-09-24 19:51:17 +0900

    追加

---
+++ AbondonInclude.php
@@ -0,0 +1,8 @@
+<?php
+echo "I'm Alive!!\n";
+
+function foo(){
+
+    echo "I'm in the foo.\n";
+}
+
---
+++ Strings.php
@@ -0,0 +1,6 @@
+<?php
+
+$filename = 'mynameisFooBar.';
+$filename = substr($filename,1,strlen($filename)-2);
+
+echo $filename;
